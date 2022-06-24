0d11185957d645161503cf59d6aecea0031eb392
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-06-24 14:23:46 +0000

    First iteration of ExcuseGenerator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var excuse =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 3)] +
+    " " +
+    when[Math.floor(Math.random() * 5)];
+
+  return excuse;
+}
+
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  const p = document.querySelector("#excuse");
+  p.innerText = excuseGenerator();
 };
